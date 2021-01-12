2f7c3fdccce16316380aa0af6fbb35540991ba13
Umpz <46953365+PeerawatUmp@users.noreply.github.com>
2021-01-13 00:14:09 +0700

    implement roll dice logic

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1');
+const current0Element = document.getElementById('current--0');
+const current1Element = document.getElementById('current--1');
+const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let currentScore = 0;
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
+
+btnRoll.addEventListener('click', () => {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceElement.classList.remove('hidden');
+  diceElement.src = `dice-${dice}.png`;
+  if (dice !== 1) {
+    currentScore += dice;
+    current0Element.textContent = currentScore;
+  } else {
+  }
+});
